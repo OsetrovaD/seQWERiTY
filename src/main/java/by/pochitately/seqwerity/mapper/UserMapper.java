48abb678f3a57ded5e1135e7def56a567e77581f
Darya Osetrova <liberty_20_07@mail.ru>
2021-11-02 12:26:51 +0300

    TG-19: user mapper to interface

--- src/main/java/by/pochitately/seqwerity/mapper/UserMapper.java
+++ src/main/java/by/pochitately/seqwerity/mapper/UserMapper.java
@@ -7,5 +7,5 @@ import org.mapstruct.Mapper;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-public abstract class UserMapper implements BaseMapper<User, UserRequest, UserResponse> {
+public interface UserMapper extends BaseMapper<User, UserRequest, UserResponse> {
 }
